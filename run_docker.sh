edb6b19a29636fbfd8c40529437a3eed77c75ab7
Cristian Baldi <bld.cris.96@gmail.com>
2016-04-07 19:48:54 +0000

    fixed --rm option

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -ti -e --rm PYWIKIBOT_LANG=$1 -e PASSWORD=$2 -e MODE=$3 -e MINUTES=$4 -e TG_CHAT_ID="@wtlpdfcheck" -e TG_API_KEY=$5 \
+docker run -ti --rm -e PYWIKIBOT_LANG=$1 -e PASSWORD=$2 -e MODE=$3 -e MINUTES=$4 -e TG_CHAT_ID="@wtlpdfcheck" -e TG_API_KEY=$5 \
     -e BOOK_URL=$6 -e PAGE_NAME=$6   wikitolearn/pdfcheck:0.3
